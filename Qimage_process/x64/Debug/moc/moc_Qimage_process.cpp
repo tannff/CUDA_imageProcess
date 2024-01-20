355359229fe2dcc50c1b5c661d9b9b2d7d1bf134
tannff <tanxinyi9903@163.com>
2024-01-20 22:33:54 +0800

    add moc.cpp

--- Qimage_process/x64/Debug/moc/moc_Qimage_process.cpp
+++ Qimage_process/x64/Debug/moc/moc_Qimage_process.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Qimage_process_t {
-    QByteArrayData data[21];
-    char stringdata0[267];
+    QByteArrayData data[31];
+    char stringdata0[389];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -41,27 +41,41 @@ QT_MOC_LITERAL(5, 64, 11), // "image_angle"
 QT_MOC_LITERAL(6, 76, 12), // "image_thresh"
 QT_MOC_LITERAL(7, 89, 15), // "image_demarcate"
 QT_MOC_LITERAL(8, 105, 9), // "image_ROI"
-QT_MOC_LITERAL(9, 115, 13), // "display_speed"
-QT_MOC_LITERAL(10, 129, 13), // "display_width"
-QT_MOC_LITERAL(11, 143, 11), // "show_resize"
-QT_MOC_LITERAL(12, 155, 10), // "show_angle"
-QT_MOC_LITERAL(13, 166, 11), // "show_thresh"
-QT_MOC_LITERAL(14, 178, 14), // "show_demarcate"
-QT_MOC_LITERAL(15, 193, 8), // "show_ROI"
-QT_MOC_LITERAL(16, 202, 13), // "online_status"
-QT_MOC_LITERAL(17, 216, 14), // "offline_status"
-QT_MOC_LITERAL(18, 231, 13), // "show_the_file"
-QT_MOC_LITERAL(19, 245, 8), // "consumer"
-QT_MOC_LITERAL(20, 254, 12) // "timer_Update"
+QT_MOC_LITERAL(9, 115, 9), // "pre_image"
+QT_MOC_LITERAL(10, 125, 13), // "display_speed"
+QT_MOC_LITERAL(11, 139, 13), // "display_width"
+QT_MOC_LITERAL(12, 153, 11), // "show_resize"
+QT_MOC_LITERAL(13, 165, 10), // "show_angle"
+QT_MOC_LITERAL(14, 176, 11), // "show_thresh"
+QT_MOC_LITERAL(15, 188, 14), // "show_demarcate"
+QT_MOC_LITERAL(16, 203, 8), // "show_ROI"
+QT_MOC_LITERAL(17, 212, 13), // "online_status"
+QT_MOC_LITERAL(18, 226, 14), // "offline_status"
+QT_MOC_LITERAL(19, 241, 13), // "show_the_file"
+QT_MOC_LITERAL(20, 255, 8), // "consumer"
+QT_MOC_LITERAL(21, 264, 10), // "new_window"
+QT_MOC_LITERAL(22, 275, 10), // "default_on"
+QT_MOC_LITERAL(23, 286, 9), // "save_file"
+QT_MOC_LITERAL(24, 296, 13), // "rotated_image"
+QT_MOC_LITERAL(25, 310, 12), // "return_image"
+QT_MOC_LITERAL(26, 323, 9), // "close_app"
+QT_MOC_LITERAL(27, 333, 10), // "gray_image"
+QT_MOC_LITERAL(28, 344, 12), // "thresh_image"
+QT_MOC_LITERAL(29, 357, 12), // "timer_Update"
+QT_MOC_LITERAL(30, 370, 18) // "offline_image_down"
 
     },
     "Qimage_process\0image_proccess_speed\0"
     "\0width_measure\0image_resize\0image_angle\0"
     "image_thresh\0image_demarcate\0image_ROI\0"
-    "display_speed\0display_width\0show_resize\0"
-    "show_angle\0show_thresh\0show_demarcate\0"
-    "show_ROI\0online_status\0offline_status\0"
-    "show_the_file\0consumer\0timer_Update"
+    "pre_image\0display_speed\0display_width\0"
+    "show_resize\0show_angle\0show_thresh\0"
+    "show_demarcate\0show_ROI\0online_status\0"
+    "offline_status\0show_the_file\0consumer\0"
+    "new_window\0default_on\0save_file\0"
+    "rotated_image\0return_image\0close_app\0"
+    "gray_image\0thresh_image\0timer_Update\0"
+    "offline_image_down"
 };
 #undef QT_MOC_LITERAL
 
@@ -71,35 +85,45 @@ static const uint qt_meta_data_Qimage_process[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      19,   14, // methods
+      29,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       7,       // signalCount
+       8,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,   12,  109,    2, 0x06 /* Public */,
-       3,    2,  134,    2, 0x06 /* Public */,
-       4,    2,  139,    2, 0x06 /* Public */,
-       5,    1,  144,    2, 0x06 /* Public */,
-       6,    1,  147,    2, 0x06 /* Public */,
-       7,    1,  150,    2, 0x06 /* Public */,
-       8,    2,  153,    2, 0x06 /* Public */,
+       1,   12,  159,    2, 0x06 /* Public */,
+       3,    2,  184,    2, 0x06 /* Public */,
+       4,    2,  189,    2, 0x06 /* Public */,
+       5,    1,  194,    2, 0x06 /* Public */,
+       6,    1,  197,    2, 0x06 /* Public */,
+       7,    1,  200,    2, 0x06 /* Public */,
+       8,    2,  203,    2, 0x06 /* Public */,
+       9,    1,  208,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       9,   12,  158,    2, 0x0a /* Public */,
-      10,    2,  183,    2, 0x0a /* Public */,
-      11,    0,  188,    2, 0x0a /* Public */,
-      12,    0,  189,    2, 0x0a /* Public */,
-      13,    0,  190,    2, 0x0a /* Public */,
-      14,    0,  191,    2, 0x0a /* Public */,
-      15,    0,  192,    2, 0x0a /* Public */,
-      16,    0,  193,    2, 0x0a /* Public */,
-      17,    0,  194,    2, 0x0a /* Public */,
-      18,    0,  195,    2, 0x0a /* Public */,
-      19,    0,  196,    2, 0x0a /* Public */,
-      20,    0,  197,    2, 0x08 /* Private */,
+      10,   12,  211,    2, 0x0a /* Public */,
+      11,    2,  236,    2, 0x0a /* Public */,
+      12,    0,  241,    2, 0x0a /* Public */,
+      13,    0,  242,    2, 0x0a /* Public */,
+      14,    0,  243,    2, 0x0a /* Public */,
+      15,    0,  244,    2, 0x0a /* Public */,
+      16,    0,  245,    2, 0x0a /* Public */,
+      17,    0,  246,    2, 0x0a /* Public */,
+      18,    0,  247,    2, 0x0a /* Public */,
+      19,    0,  248,    2, 0x0a /* Public */,
+      20,    0,  249,    2, 0x0a /* Public */,
+      21,    0,  250,    2, 0x0a /* Public */,
+      22,    0,  251,    2, 0x0a /* Public */,
+      23,    0,  252,    2, 0x0a /* Public */,
+      24,    1,  253,    2, 0x0a /* Public */,
+      25,    1,  256,    2, 0x0a /* Public */,
+      26,    0,  259,    2, 0x0a /* Public */,
+      27,    0,  260,    2, 0x0a /* Public */,
+      28,    0,  261,    2, 0x0a /* Public */,
+      29,    0,  262,    2, 0x08 /* Private */,
+      30,    0,  263,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
@@ -109,6 +133,7 @@ static const uint qt_meta_data_Qimage_process[] = {
     QMetaType::Void, QMetaType::Int,    2,
     QMetaType::Void, QMetaType::Int,    2,
     QMetaType::Void, QMetaType::Int, QMetaType::Int,    2,    2,
+    QMetaType::Void, QMetaType::QImage,    2,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Double, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float, QMetaType::Float,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
@@ -118,6 +143,15 @@ static const uint qt_meta_data_Qimage_process[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Float,    2,
+    QMetaType::Void, QMetaType::QImage,    2,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -140,18 +174,28 @@ void Qimage_process::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
         case 4: _t->image_thresh((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 5: _t->image_demarcate((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 6: _t->image_ROI((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
-        case 7: _t->display_speed((*reinterpret_cast< double(*)>(_a[1])),(*reinterpret_cast< double(*)>(_a[2])),(*reinterpret_cast< double(*)>(_a[3])),(*reinterpret_cast< double(*)>(_a[4])),(*reinterpret_cast< double(*)>(_a[5])),(*reinterpret_cast< double(*)>(_a[6])),(*reinterpret_cast< float(*)>(_a[7])),(*reinterpret_cast< float(*)>(_a[8])),(*reinterpret_cast< float(*)>(_a[9])),(*reinterpret_cast< float(*)>(_a[10])),(*reinterpret_cast< float(*)>(_a[11])),(*reinterpret_cast< float(*)>(_a[12]))); break;
-        case 8: _t->display_width((*reinterpret_cast< float(*)>(_a[1])),(*reinterpret_cast< double(*)>(_a[2]))); break;
-        case 9: _t->show_resize(); break;
-        case 10: _t->show_angle(); break;
-        case 11: _t->show_thresh(); break;
-        case 12: _t->show_demarcate(); break;
-        case 13: _t->show_ROI(); break;
-        case 14: _t->online_status(); break;
-        case 15: _t->offline_status(); break;
-        case 16: _t->show_the_file(); break;
-        case 17: _t->consumer(); break;
-        case 18: _t->timer_Update(); break;
+        case 7: _t->pre_image((*reinterpret_cast< QImage(*)>(_a[1]))); break;
+        case 8: _t->display_speed((*reinterpret_cast< double(*)>(_a[1])),(*reinterpret_cast< double(*)>(_a[2])),(*reinterpret_cast< double(*)>(_a[3])),(*reinterpret_cast< double(*)>(_a[4])),(*reinterpret_cast< double(*)>(_a[5])),(*reinterpret_cast< double(*)>(_a[6])),(*reinterpret_cast< float(*)>(_a[7])),(*reinterpret_cast< float(*)>(_a[8])),(*reinterpret_cast< float(*)>(_a[9])),(*reinterpret_cast< float(*)>(_a[10])),(*reinterpret_cast< float(*)>(_a[11])),(*reinterpret_cast< float(*)>(_a[12]))); break;
+        case 9: _t->display_width((*reinterpret_cast< float(*)>(_a[1])),(*reinterpret_cast< double(*)>(_a[2]))); break;
+        case 10: _t->show_resize(); break;
+        case 11: _t->show_angle(); break;
+        case 12: _t->show_thresh(); break;
+        case 13: _t->show_demarcate(); break;
+        case 14: _t->show_ROI(); break;
+        case 15: _t->online_status(); break;
+        case 16: _t->offline_status(); break;
+        case 17: _t->show_the_file(); break;
+        case 18: _t->consumer(); break;
+        case 19: _t->new_window(); break;
+        case 20: _t->default_on(); break;
+        case 21: _t->save_file(); break;
+        case 22: _t->rotated_image((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 23: _t->return_image((*reinterpret_cast< QImage(*)>(_a[1]))); break;
+        case 24: _t->close_app(); break;
+        case 25: _t->gray_image(); break;
+        case 26: _t->thresh_image(); break;
+        case 27: _t->timer_Update(); break;
+        case 28: _t->offline_image_down(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -205,6 +249,13 @@ void Qimage_process::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
                 return;
             }
         }
+        {
+            using _t = void (Qimage_process::*)(QImage );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Qimage_process::pre_image)) {
+                *result = 7;
+                return;
+            }
+        }
     }
 }
 
@@ -237,13 +288,13 @@ int Qimage_process::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 19)
+        if (_id < 29)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 19;
+        _id -= 29;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 19)
+        if (_id < 29)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 19;
+        _id -= 29;
     }
     return _id;
 }
@@ -295,6 +346,13 @@ void Qimage_process::image_ROI(int _t1, int _t2)
 {
     void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))) };
     QMetaObject::activate(this, &staticMetaObject, 6, _a);
+}
+
+// SIGNAL 7
+void Qimage_process::pre_image(QImage _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 7, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
